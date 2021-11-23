0912c915bd91043b0a50b43b9069cc33ad6afc1d
Tim Vrakas <timv@stanford.edu>
2021-11-23 00:54:12 -0800

    now with working saml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root to: "application#index"
+  get 'logged_in', to: "application#logged_in"
+  post 'saml_login', to: "application#saml_login"
+  post 'saml_callback', to: "application#saml_callback"
 end
---
+++ db/migrate/20211123083913_add_users.rb
@@ -0,0 +1,14 @@
+class AddUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email, null: false, default: ""
+      
+
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
