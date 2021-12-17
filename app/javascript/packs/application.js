524317edbd5ec1ae7dcef6991ee0a76313c14f69
Tim Vrakas <timv@stanford.edu>
2021-12-17 02:59:41 -0700

    PDF download and image scaling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,18 @@ ActiveStorage.start()
 
 require("trix")
 require("@rails/actiontext")
+
+window.addEventListener("trix-file-accept", function(event) {
+    const maxFileSize = 2 * 1024 * 1024 // 2MB 
+    if (event.file.size > maxFileSize) {
+        event.preventDefault()
+        alert("Only support attachment files upto size 1MB!")
+    }
+
+    const acceptedTypes = ['image/jpeg', 'image/png', 'application/pdf']
+    if (!acceptedTypes.includes(event.file.type)) {
+      event.preventDefault()
+      alert("Only support attachment of JPEG, PNG, PDF files")
+    }
+
+  })
