79d5829741e249e3d7aebdfa226c865b4c6ee81c
Tim Vrakas <timv@stanford.edu>
2022-03-22 15:27:09 -0700

    fix enums

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+    include TranslateEnum
+    
     devise :timeoutable
-    enum member_type: [:neither, :grad, :undergrad], _default: "neither"
-    enum ug_year: {"Frosh":1, "Sophmore":2, "Junior":3, "Senior":4, "Senior+":5}
+
+    enum member_type: {neither:nil, grad:1, undergrad:2}, _default: :neither
+    translate_enum :member_type
+
+    enum ug_year: { na:nil, year1:1, year2:2, year3:3, year4:4, year5:5 }
+    translate_enum :ug_year
+
     has_many :petitions
     has_and_belongs_to_many :signatures,
         class_name: 'Petition',
