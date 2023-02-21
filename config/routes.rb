38b532fa5c325c1409bc56de6e4ab36292d4de93
Tim Vrakas <timv@stanford.edu>
2023-02-20 21:21:37 -0800

    try this

--- config/routes.rb
+++ config/routes.rb
@@ -18,10 +18,10 @@ Rails.application.routes.draw do
 
   root to: "application#index"
   get 'logged_in', to: "application#logged_in"
-  post 'saml_login', to: "application#saml_login"
+  get 'saml_login', to: "application#saml_login"
   post 'saml_callback', to: "application#saml_callback"
   get 'saml_metadata.xml', to: "application#metadata"
-  post 'saml_logout', to:"application#saml_logout"
+  get 'saml_logout', to:"application#saml_logout"
   
   get '/:slug', to: "petitions#by_slug" #this needs to go last, so that custom slugs can't break things
   post '/:slug', to: "petitions#sign" #this needs to go last, so that custom slugs can't break things
